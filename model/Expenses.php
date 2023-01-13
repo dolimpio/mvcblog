bff38c9c0179d257ef6cd256c85b8d1e947da8d3
dolimpio <olimpiosilvadavid@gmail.com>
2023-01-13 19:59:15 +0100

    modificaciones

--- model/Expenses.php
+++ model/Expenses.php
@@ -215,8 +215,8 @@ class expenses {
 			$errors["expense_type"] = "type is mandatory";
 		}
 			//combustible, alimentacion, comunicaciones, suministros, ocio
-		if ($this->expense_type != "Combustible" && $this->expense_type != "Alimentacion" && $this->expense_type != "Comunicaciones" &&
-		$this->expense_type != "Suministros" && $this->expense_type != "Ocio" && $this->expense_type != NULL) {
+		if ($this->expense_type != "combustible" && $this->expense_type != "alimentacion" && $this->expense_type != "comunicaciones" &&
+		$this->expense_type != "suministros" && $this->expense_type != "ocio" && $this->expense_type != NULL) {
 			$errors["expense_type"] = "type must be combustible, alimentacion, comunicaciones, suministros or ocio";
 		}
 		if ($this->expense_date == NULL ) {
